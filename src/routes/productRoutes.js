eef57f061c3e608ff898b68faad815d9daafca87
Felipe Diez <juanfelipediezc@gmail.com>
2022-02-10 21:09:01 -0500

    actualizando register y JSON

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -5,6 +5,7 @@ const pController = require('../controllers/productControllers');
 
 router.get('/',pController.productos);
 router.get('/createProduct',pController.crear);
+
 router.get('/productCart',pController.productCart);
 
 
