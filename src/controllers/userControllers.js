9b4d2988102e7c89466192bd567d595ac730d0cb
Felipe Diez <juanfelipediezc@gmail.com>
2022-03-06 21:38:03 -0500

    Actualizando Usuarios

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,8 +1,34 @@
 const fs = require('fs');
+const path = require('path');
+const { validationResult } = require('express-validator');
+
+const usersFilePath = path.join(__dirname, '../data/users.json');
+const usuarios = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
 const usuariosController = {
     register: (req,res) => {
         res.render('register.ejs');
+    },
+    create: (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.render('createProduct', { errors: errors.mapped(), old: req.body })
+        } else {
+            let image
+            if (req.file != undefined) {
+                image = req.file.filename
+            } else {
+                image = '/users/404-user.jpg'
+            }
+            let nuevoUser = {
+                id: usuarios[usuarios.length -1].id +1,
+                ...req.body,
+                image
+            }
+            let usuarioNuevo = [...usuarios, nuevoUser]
+            fs.writeFileSync(usersFilePath, JSON.stringify(usuarioNuevo, null, ''));
+            res.redirect('/');
+        }
     },
     login:(req,res)=> {
         res.render('login.ejs');
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const usuariosController = require('../controllers/userControllers');
+const multer = require('multer');
+const path = require('path');
+
+//multer para imagenes
+
+var storage = multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null, 'public/images/users')
+    },
+    filename: function(req,file,cb){
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+})
+var upload = multer({storage})
+
+
 
 router.get('/register', usuariosController.register);
+router.post('/',upload.single('image'), usuariosController.create);
 router.get('/login',usuariosController.login);
 
 
