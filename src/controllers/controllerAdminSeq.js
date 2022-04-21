2ac5974c8691915f831637ad3d131320c40bd1e4
Felipe Diez <juanfelipediezc@gmail.com>
2022-04-21 18:38:13 -0500

    DMejorando el diseño del administrador de productos

--- src/controllers/controllerAdminSeq.js
+++ src/controllers/controllerAdminSeq.js
@@ -96,45 +96,4 @@ const controllerAdminSeq = {
 
 
 
-
-/*const controllerAdminSeqPrueba = {
-  index: (req,res) =>{
-   db.Producto.findAll()
-     .then(function(productos){
-       res.render('listProducts', {productos});
-     })
-     .catch(error => res.send(error))
-  },
-  create: (req,res) => {
-   db.Cepa.findAll()
-     .then(function(cepas){
-       res.render("createProduct", {cepas});
-     })
-  },
- save: (req,res) => {
-   db.Producto.create({
-     nombre: req.body.nombre,
-     precio: req.body.precio,
-     cepa_id: req.body.cepa,
-     categoria: req.body.categoria,
-     descripcion: req.body.descripcion,
-     imagen: req.file.filename
-     
-   })
-   .then(vinos => {
-     res.redirect("/administrar");
-   })
-   .catch(error => res.send(error))
- },
- show: (req,res)=>{
-   db.Producto.findByPk(req.params.id, {
-     include : [{association : 'cepa'}]
-   })
-   .then(miVino=> {
-     res.render('productDetail', {miVino})
-   })
-   .catch(error => res.send(error))}
-}*/
-
 module.exports = controllerAdminSeq;
-//module.exports = controllerAdminSeqPrueba;
