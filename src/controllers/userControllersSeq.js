40870da6beecec51c59a50a8ab3cbf91a104e917
Daniel Modarelli <modarellidaniel@gmail.com>
2022-04-28 12:46:30 -0300

    Migrando USER a sequelize

--- src/controllers/userControllersSeq.js
+++ src/controllers/userControllersSeq.js
@@ -2,22 +2,110 @@ const path = require('path');
 const { devNull } = require('os');
 const db = require('../../src/database/models');
 const res = require('express/lib/response');
+const { validationResult } = require('express-validator');
+const bcryptjs = require('bcryptjs');
 const Op = db.Sequelize.Op;
 
 const userControllersSeq = {
-   index: (req,res) =>{
-    db.Usuario.findAll()
-      .then(function(usuarios){ 
-        res.render('listUsers', {usuarios});
-      })
-      .catch(error => res.send(error))
-   },
-   create: (req,res) => {
+  register: (req,res) => { 
+  res.render('register.ejs');
+  },
+
+  processRegister: (req, res) => {
+        
+    const errors = validationResult(req);
+    
+    if (!errors.isEmpty()) {
+        return res.render('register', { errors: errors.mapped(), oldData: req.body })
+    }
+   
+    /*Busco si el usuario esta en la base para no repetir el email */
+
+    let userInDb = User.findByField('email',req.body.email)
+
+    if (userInDb) {
+        return res.render('register', {
+                    errors: { 
+                        email: {
+                            msg: 'El mail se encuentra registrado'
+                        }
+                    },
+             oldData: req.body
+         })
+    }
+
+    let userToCreate = {
+        ...req.body, 
+        password: bcryptjs.hashSync(req.body.password,10),
+        image: req.file.filename,
+    }
+
+     delete userToCreate.cpassword,
+     User.create(userToCreate);
+     return res.redirect('/user/login')
+    
+  },
+
+  login: (req,res) => { 
+         
+    res.render('login.ejs');
+    
+  },
+
+  authenticate: (req, res) => { 
+       
+    // return res.send(req.body);
+
+     let userToLogin = User.findByField('email' , req.body.email);
+          
+    if (userToLogin){
+
+         let isOkpassword = bcryptjs.compareSync(req.body.password, userToLogin.password);
+
+        if (isOkpassword){
+             delete userToLogin.password;    
+             
+             req.session.userLogeado = userToLogin;
+
+             /*  GUARDO LA COOKIE*/
+             if (req.body.recordar){
+                 res.cookie('userMail', req.body.email , {maxAge: (1000 * 60) * 2})
+             }
+
+            return res.redirect('/');
+         } else {
+            return res.render('login.ejs',{
+              errors: {
+              email:{ msg: 'Las credenciales no son válidos' }
+              }   
+            });
+          }
+     } 
+  },
+
+  logout: (req, res) =>{
+    res.clearCookie('userMail')
+    req.session.destroy();
+    return res.redirect('/');
+  },
+  
+
+  /* tabla de administracion de usuarios*/
+  index: (req,res) =>{
+      db.Usuario.findAll()
+        .then(function(usuarios){ 
+          res.render('listUsers', {usuarios});
+        })
+        .catch(error => res.send(error))
+  },
+
+  create: (req,res) => {
     db.Cepa.findAll()
       .then(function(cepas){
         res.render("createProduct", {cepas});
       })
-   },
+  },
+
   save: (req,res) => {
     db.Producto.create({
       nombre: req.body.nombre,
