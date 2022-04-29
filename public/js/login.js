6add83f907315384d3b977335df5ffde1260d6c4
Felipe Diez <juanfelipediezc@gmail.com>
2022-04-29 18:10:55 -0500

    Se corrige error de la validación del login y se crea alerta con estilo

--- public/js/login.js
+++ public/js/login.js
@@ -1,18 +1,23 @@
 
-/*
-window.addEventListener("load", function() {
-/*
-    let formulario = document.querySelector("form.create-user");
-        formulario.addEventListener("submit",function(e) {
-            e.preventDefault();
-            
-            let campoNombre = document.querySelector("email");
-    
-            if(campoNombre.value == "") {
-    
-                alert("El campo nombre no puede estar vacio")
-            }
-        });
-    });
+window.addEventListener("load", function () {
+  let formulario = document.querySelector(".create-user");
 
-    */
+  formulario.addEventListener("submit", function (e) {
+    let campoEmail = document.querySelector("#email");
+    console.log(campoEmail);
+
+    let emailReg = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+
+    if (!emailReg.test(campoEmail.value)) {
+      e.preventDefault();
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "El e-mail " + campoEmail.value + " no es e-mail valido",
+    }); 
+    }
+
+
+    
+  });
+});
