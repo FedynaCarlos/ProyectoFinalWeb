a14b30524697b7a40f2619992d2c9a74aa4d00d9
Daniel Modarelli <modarellidaniel@gmail.com>
2022-05-19 08:52:06 -0300

    nada

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -3,7 +3,7 @@ const { devNull } = require("os");
 module.exports = {
   development: {
     username: "root",
-    password: '8090',
+    password: null,
     database: "grupo_5",
     host: "127.0.0.1",
     dialect: "mysql",
